5c28f32d64f081e9507f0925747bb3bfb7b50435
Dakota Fabro <dakota@weareoutright.com>
2024-10-29 20:34:09 -0700

    added page transitions - still need to fix previous swipe to be opposite the next swipe

--- app/our-work/ClientWorkPageLayout.jsx
+++ app/our-work/ClientWorkPageLayout.jsx
@@ -13,6 +13,8 @@ import ClientProjectOverview from "./components/ClientProjectOverview";
 import ClientMainHeadline from "./components/ClientMainHeadline";
 import ClientFullWidthImg from "./components/ClientFullWidthImg";
 import { PrevNextBottomNav } from "./components/PrevNextBottomNav";
+import PageTransition from "../components/PageTransition";
+import { useRouter } from "next/navigation";
 
 const ClientWorkPageLayout = ({ clientProject }) => {
   const [currentWaypoint, setCurrentWaypoint] = useState("");
@@ -20,7 +22,9 @@ const ClientWorkPageLayout = ({ clientProject }) => {
   // State for resolved prev_page and next_page
   const [prevPage, setPrevPage] = useState(null);
   const [nextPage, setNextPage] = useState(null);
+  const [xOffset, setXOffset] = useState(1000);
 
+  const router = useRouter();
   const clientWorkPageContainerRef = useRef(null);
   const pageComponentsRef = useRef(null);
   const contactRef = useRef(null);
@@ -36,6 +40,11 @@ const ClientWorkPageLayout = ({ clientProject }) => {
     _prev_page,
     _next_page,
   } = clientProject;
+
+  const handleNavigation = (slug, xOffsetDirection) => {
+    setXOffset(xOffsetDirection); // Update direction
+    router.push(slug);
+  };
 
   useEffect(() => {
     // Resolve async prev_page and next_page
@@ -60,13 +69,8 @@ const ClientWorkPageLayout = ({ clientProject }) => {
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            const { waypoint, order } = entry.target.dataset;
+            const { waypoint } = entry.target.dataset;
             setCurrentWaypoint(waypoint);
-            setCurrentOrder(order);
-
-            if (waypoint === "Contact") {
-              setSiteHidelineDisplay("none");
-            }
           }
         });
       },
@@ -94,52 +98,61 @@ const ClientWorkPageLayout = ({ clientProject }) => {
   }, []);
 
   return (
-    <div className="ClientWorkPage" ref={clientWorkPageContainerRef}>
-      <WorkGalleryUtility waypoint={currentWaypoint} />
-      <div ref={pageComponentsRef} data-waypoint="WorkPageComponent">
-        <ClientWorkHero
-          heroBg={hero.hero_bg_img}
-          waypoint="WorkPageComponent"
-        />
-        <ClientWorkHeroHeader
-          heroHeader={hero.header}
-          waypoint="WorkPageComponent"
-        />
-        <ClientMainHeadline
-          mainHeadline={main_headline}
-          waypoint="WorkPageComponent"
-        />
-        <ClientWorkBento
-          bentoImages={bento_images}
-          waypoint="WorkPageComponent"
-        />
-        <ClientQuote clientQuote={client_quote} waypoint="WorkPageComponent" />
-        <ClientFullWidthImg
-          fullWidthImg={full_width_img}
-          waypoint="WorkPageComponent"
-        />
-        <ClientSpotlight
-          clientSpotlight={client_spotlight}
-          waypoint="WorkPageComponent"
-        />
-        <ClientProjectOverview
-          projectOverview={project_overview}
-          waypoint="WorkPageComponent"
-        />
-        <PrevNextBottomNav
-          prevPage={prevPage}
-          nextPage={nextPage}
-          waypoint="WorkPageComponent"
-        />
+    <PageTransition page={hero.header.project_title} xOffset={xOffset}>
+      <div className="ClientWorkPage" ref={clientWorkPageContainerRef}>
+        <WorkGalleryUtility waypoint={currentWaypoint} />
+        <div ref={pageComponentsRef} data-waypoint="WorkPageComponent">
+          <ClientWorkHero
+            heroBg={hero.hero_bg_img}
+            waypoint="WorkPageComponent"
+          />
+          <ClientWorkHeroHeader
+            heroHeader={hero.header}
+            waypoint="WorkPageComponent"
+          />
+          <ClientMainHeadline
+            mainHeadline={main_headline}
+            waypoint="WorkPageComponent"
+          />
+          <ClientWorkBento
+            bentoImages={bento_images}
+            waypoint="WorkPageComponent"
+          />
+          <ClientQuote
+            clientQuote={client_quote}
+            waypoint="WorkPageComponent"
+          />
+          <ClientFullWidthImg
+            fullWidthImg={full_width_img}
+            waypoint="WorkPageComponent"
+          />
+          <ClientSpotlight
+            clientSpotlight={client_spotlight}
+            waypoint="WorkPageComponent"
+          />
+          <ClientProjectOverview
+            projectOverview={project_overview}
+            waypoint="WorkPageComponent"
+          />
+          <PrevNextBottomNav
+            prevPage={prevPage}
+            nextPage={nextPage}
+            waypoint="WorkPageComponent"
+            handleNavigation={handleNavigation}
+          />
+        </div>
+        <div
+          ref={contactRef}
+          data-waypoint={ContactPaneProps.waypoint}
+          data-order={ContactPaneProps.order}
+        >
+          <PaneOuter
+            pane={ContactPaneProps}
+            currentWaypoint={currentWaypoint}
+          />
+        </div>
       </div>
-      <div
-        ref={contactRef}
-        data-waypoint={ContactPaneProps.waypoint}
-        data-order={ContactPaneProps.order}
-      >
-        <PaneOuter pane={ContactPaneProps} currentWaypoint={currentWaypoint} />
-      </div>
-    </div>
+    </PageTransition>
   );
 };
 
