07fb9cbc3d28cd21d6a3f343c544fe45a8fbdc01
Dakota Fabro <dakota@weareoutright.com>
2024-10-31 16:51:24 -0700

    implement vote page

--- app/vote/page.jsx
+++ app/vote/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import WorkGalleryUtility from "../components/WorkGalleryUtility";
+import VoteGallery from "./VoteGallery";
 
 const VotePage = () => {
-  return <div>VotePage</div>;
+  return (
+    <div className="WorkPage VotePage">
+      <WorkGalleryUtility />
+      <VoteGallery />
+    </div>
+  );
 };
 
 export default VotePage;
