d9495f4c991ae6237d6d33bcf509fa0dd08a80fc
Dakota Fabro <dakota@weareoutright.com>
2024-10-31 15:03:35 -0700

    build video page

--- app/video/page.jsx
+++ app/video/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import VideoGallery from "./VideoGallery";
+import WorkGalleryUtility from "../components/WorkGalleryUtility";
 
 const VideoPage = () => {
-  return <div>VideoPage</div>;
+  return (
+    <div className="WorkPage">
+      <WorkGalleryUtility />
+      <VideoGallery />
+    </div>
+  );
 };
 
 export default VideoPage;
---
+++ app/video/videos.js
@@ -0,0 +1,79 @@
+const VIDEOS = [
+  {
+    title: "We See...",
+    client: "Rails to Trails Conservancy",
+    embedURL: "https://player.vimeo.com/video/948843237",
+  },
+  {
+    title: "From Day To Night",
+    client: "Experience Fayetteville",
+    embedURL: "https://player.vimeo.com/video/1012511822",
+  },
+  {
+    title: "Brand Reveal",
+    client: "GNRT.AI",
+    embedURL: "https://player.vimeo.com/video/908828103",
+  },
+  {
+    title: "It's the Little Things",
+    client: "PAIC",
+    embedURL: "https://player.vimeo.com/video/884554940",
+  },
+  {
+    title: "Super Bowl Ad Blitz",
+    client: "YouTube",
+    embedURL: "https://player.vimeo.com/video/749650076",
+  },
+  {
+    title: "Beauty Festival",
+    client: "YouTube",
+    embedURL: "https://player.vimeo.com/video/749646568",
+  },
+  {
+    title: "Ads Creative Studio",
+    client: "Google",
+    embedURL: "https://player.vimeo.com/video/749647892",
+  },
+  {
+    title: "Black Life",
+    client: "ONE VIP",
+    embedURL: "https://player.vimeo.com/video/523712738",
+  },
+  {
+    title: "2022 BeyGood Internship",
+    client: "Adidas x Ivy Park",
+    embedURL: "https://player.vimeo.com/video/663133892",
+  },
+  {
+    title: "Let's Crush Covid",
+    client: "Montgomery County Immunization Coalition",
+    embedURL: "https://player.vimeo.com/video/632822939",
+  },
+  {
+    title: "The Thread",
+    client: "Marriott International",
+    embedURL: "https://player.vimeo.com/video/533588781",
+  },
+  {
+    title: "Welcome to the Family Room",
+    client: "The Family Room",
+    embedURL: "https://player.vimeo.com/video/523708281",
+  },
+  {
+    title: "Wish List Live",
+    client: "Google/YouTube",
+    embedURL: "https://player.vimeo.com/video/663083072",
+  },
+  {
+    title: "Fueler",
+    client: "Growth Energy",
+    embedURL: "https://player.vimeo.com/video/566690657",
+  },
+  {
+    title: "I Got The Flu Shot",
+    client: "Montgomery County Immunization Coalition",
+    embedURL: "https://player.vimeo.com/video/663078290",
+  },
+];
+
+export default VIDEOS;
