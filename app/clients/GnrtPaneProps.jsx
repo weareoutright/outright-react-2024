a1288d3f1e98e513f349dc6e9642db6e19e1408f
Gabriela Campos <gabriela@Gabrielas-MacBook-Outright.local>
2024-11-19 19:55:49 -0800

    changes and additions to homepage client rows

--- app/clients/GnrtPaneProps.jsx
+++ app/clients/GnrtPaneProps.jsx
@@ -11,13 +11,14 @@ export const pane = {
   background: "img",
   background_video: "900197626",
   responsive_background_video: "900798479",
-  order: "03",
+  order: "08",
   waypoint: "Featured",
+  background_image: "bg-gnrt-video.jpg",
   iframe: (
     <Iframe
       src="https://player.vimeo.com/video/900197626?badge=0&amp;autopause=0&amp;player_id=0&amp;autoplay=1&amp;loop=1&amp;controls=0&amp;responsive=1&amp;background=1&amp;app_id=58479"
       frameborder="0"
-      title="Google_YT_Panel"
+      title="GNRT_Panel"
     ></Iframe>
   ),
   hide_title: false,
---
+++ app/clients/RtcPane.jsx
@@ -0,0 +1,9 @@
+const RtcPane = () => {
+    return (
+        <p>
+            A new brand identity that smells like a day outside, feels like a breezy bike ride, and moves as boldly as the organization’s mission to re-imagine public spaces.
+        </p>
+    );
+};
+
+export default RtcPane;
