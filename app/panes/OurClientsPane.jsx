1d235033fc6206bda1c20ba72924a637cfa555e8
Dakota Fabro <dakota@weareoutright.com>
2024-12-10 12:33:22 -0800

    added new logos to clients pane

--- app/panes/OurClientsPane.jsx
+++ app/panes/OurClientsPane.jsx
@@ -4,9 +4,12 @@ import MARRIOTT from "../assets/client-logos/marriott.svg";
 import ADIDAS from "../assets/client-logos/adidas.svg";
 import OC from "../assets/client-logos/oc.svg";
 import ME_TOO from "../assets/client-logos/me-too.svg";
-import AARP from "../assets/client-logos/aarp.svg";
-import SHARE_OUR_STRENGTH from "../assets/client-logos/share-our-strength.svg";
 import WRI from "../assets/client-logos/wri.svg";
+import NHL from "../assets/client-logos/nhl.svg";
+import EF from "../assets/client-logos/ef.svg";
+import KABOOM from "../assets/client-logos/kaboom.svg";
+import RTC from "../assets/client-logos/rtc.svg";
+import RWJ from "../assets/client-logos/rwj.svg";
 
 const OurClientsPane = () => {
   return (
@@ -16,12 +19,15 @@ const OurClientsPane = () => {
         <YOUTUBE className="youtube-logo" />
         <GOOGLE className="google-logo" />
         <MARRIOTT className="marriott-logo" />
-        <ADIDAS className="adidas-logo" />
+        <NHL className="nhl-logo" />
         <OC className="oc-logo" />
         <ME_TOO className="me-too-logo" />
-        <AARP className="aarp-logo" />
-        <SHARE_OUR_STRENGTH className="share-our-strength-logo" />
+        <EF className="ef-logo" />
+        <KABOOM className="kaboom-logo" />
+        <RTC className="rtc-logo" />
+        <ADIDAS className="adidas-logo" />
         <WRI className="wri-logo" />
+        <RWJ className="rwj-logo" />
       </div>
     </div>
   );
