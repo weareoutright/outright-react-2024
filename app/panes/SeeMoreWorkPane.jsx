a4857b5104a406ff906ff65cf5aed4ce1d96208b
Dakota Fabro <dakota@weareoutright.com>
2024-12-20 14:27:50 -0500

    fix grid and check out our work div - changed to link

--- app/panes/SeeMoreWorkPane.jsx
+++ app/panes/SeeMoreWorkPane.jsx
@@ -5,30 +5,37 @@ import { useState } from "react";
 const SeeMoreWork = () => {
   const [opacity, setOpacity] = useState(0);
   return (
-    <div className="see-more-work">
-      <div className="header-text">
-      <h3 className="pane-title">Want more?</h3>
-        <a
-          href="/our-work"
-          className="check-out-our-work-link underline"
-          onMouseEnter={() => setOpacity(1)}
-          onMouseLeave={() => setOpacity(0)}
-        >
-          <br />
-          <h2 className="work-subheading underline">
-            Check Out Our Work.
-            <SCRIBBLE_LINE_DARK style={{ opacity: opacity }} />
-          </h2>
-        </a>
-      </div>
-      <br />
+    <div className="see-more-work" id="our-work">
       <a
         href="/our-work"
-        className="right-chevron-link"
+        className="see-more-work-link-wrapper"
         onMouseEnter={() => setOpacity(1)}
         onMouseLeave={() => setOpacity(0)}
       >
-        <ICON_CHEVRON_DOWN className="right-chevron" />
+        <div className="header-text">
+          <h3 className="pane-title">Want more?</h3>
+          <a
+            href="/our-work"
+            className="check-out-our-work-link underline"
+            onMouseEnter={() => setOpacity(1)}
+            onMouseLeave={() => setOpacity(0)}
+          >
+            <br />
+            <h2 className="work-subheading underline">
+              Check Out Our Work.
+              <SCRIBBLE_LINE_DARK style={{ opacity: opacity }} />
+            </h2>
+          </a>
+        </div>
+        <br />
+        <a
+          href="/our-work"
+          className="right-chevron-link"
+          onMouseEnter={() => setOpacity(1)}
+          onMouseLeave={() => setOpacity(0)}
+        >
+          <ICON_CHEVRON_DOWN className="right-chevron" />
+        </a>
       </a>
     </div>
   );
