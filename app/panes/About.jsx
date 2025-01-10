da79a080b4b5625ddc392307495b3d8f57a4fc5f
Dakota Fabro <dakota@weareoutright.com>
2025-01-10 09:46:56 -0800

    edit escaped characters

--- app/panes/About.jsx
+++ app/panes/About.jsx
@@ -2,9 +2,24 @@ const AboutPane = () => {
   return (
     <div className="AboutPane">
       <div className="about-content">
-          <p>We are Outright, a crew of seasoned artists and strategists who are personally invested in the collective success of ourselves and our clients.</p>
-          <p>We’re a full-service creative agency that believes in design for good—working where consumer brands and nonprofits converge to make a positive impact on our world. Based in D.C., we have our pulse on the intersection of advocacy and change, while our nationwide team allows us to collaborate with clients across the country and bring diverse perspectives to every project we touch.</p>
-          <p>As a BIPOC female-founded firm, our identity is woven into everything we do, shaping work that’s thoughtful, resonant, and rooted in cultural awareness.</p>
+        <p>
+          We are Outright, a crew of seasoned artists and strategists who are
+          personally invested in the collective success of ourselves and our
+          clients.
+        </p>
+        <p>
+          We&apos;re a full-service creative agency that believes in design for
+          good—working where consumer brands and nonprofits converge to make a
+          positive impact on our world. Based in D.C., we have our pulse on the
+          intersection of advocacy and change, while our nationwide team allows
+          us to collaborate with clients across the country and bring diverse
+          perspectives to every project we touch.
+        </p>
+        <p>
+          As a BIPOC female-founded firm, our identity is woven into everything
+          we do, shaping work that&apos;s thoughtful, resonant, and rooted in
+          cultural awareness.
+        </p>
       </div>
     </div>
   );
