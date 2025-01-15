42e5d7f793f6a6adc8f428d01e016f5686eb0a7a
Dakota Fabro <dakota@weareoutright.com>
2025-01-15 13:18:37 -0800

    change work page animation to fade in and out

--- app/components/PageTransition.jsx
+++ app/components/PageTransition.jsx
@@ -7,7 +7,7 @@ const PageTransition = ({ children, page, xOffset }) => {
     <AnimatePresence>
       <motion.div
         key={page}
-        initial={{ x: xOffset, opacity: 0 }}
+        initial={{ x: 0, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         // exit={{ x: -xOffset, opacity: 0 }} // Reverse offset on exit
         transition={{ duration: 0.5, ease: "easeInOut" }}
